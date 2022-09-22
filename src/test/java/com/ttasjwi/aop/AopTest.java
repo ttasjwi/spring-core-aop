2e50938c2ac6ab56308d305d472703096e47b969
ttasjwi <ttasjwi920@gmail.com>
2022-09-22 14:59:27 +0900

    AOP 구현 1 - 시작
    - 클래스 선언부에 @Aspect를 지정하여 애스펙트 정의
    - @Around 어노테이션 속성값을 통해 포인트컷 지정
    - @Around와 메서드를 정의를 통해 Advice 생성
    - @Around가 적용된 메서드는 결국 어드바이저가 된다.

--- src/test/java/com/ttasjwi/aop/AopTest.java
+++ src/test/java/com/ttasjwi/aop/AopTest.java
@@ -2,17 +2,19 @@ package com.ttasjwi.aop;
 
 import com.ttasjwi.aop.order.OrderRepository;
 import com.ttasjwi.aop.order.OrderService;
+import com.ttasjwi.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
