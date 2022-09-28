bfdd1a0dbafbfe1a9237d4e4d7c7d2036329c322
ttasjwi <ttasjwi920@gmail.com>
2022-09-28 21:30:24 +0900

    포인트컷 지시자 - 메서드명, 패키지명

--- src/test/java/com/ttasjwi/aop/pointcut/ExecutionTest.java
+++ src/test/java/com/ttasjwi/aop/pointcut/ExecutionTest.java
@@ -3,12 +3,13 @@ package com.ttasjwi.aop.pointcut;
 import com.ttasjwi.aop.member.MemberServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
 import java.lang.reflect.Method;
 
-
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class ExecutionTest {
@@ -27,4 +28,88 @@ public class ExecutionTest {
         log.info("helloMethod = {}", helloMethod);
     }
 
+    @Test
+    @DisplayName("가장 정확한 포인트컷")
+    void exactMatch() {
+        // public java.lang.String com.ttasjwi.aop.member.MemberServiceImpl.hello(java.lang.String)
+        pointcut.setExpression("execution(public String com.ttasjwi.aop.member.MemberServiceImpl.hello(String))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("가장 많이 생략한 포인트컷")
+    void allMatch() {
+        pointcut.setExpression("execution(* *(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("메서드 이름을 맞춘 포인트컷")
+    void nameMatch() {
+        pointcut.setExpression("execution(* hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("메서드 이름 - 접두사 포인트컷")
+    void nameMatchStar1() {
+        pointcut.setExpression("execution(* hel*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("메서드 이름 중 특정 문자열이 포함된 포인트컷")
+    void nameMatchStar2() {
+        pointcut.setExpression("execution(* *ell*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("메서드 이름 - 접미사 포인트컷")
+    void nameMatchStar3() {
+        pointcut.setExpression("execution(* *llo(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("메서드 이름 - 실패")
+    void nameMatchFalse() {
+        pointcut.setExpression("execution(* nono(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    @DisplayName("패키지 이름, 클래스명, 메서드명을 정확하게 매칭")
+    void packageExactMatch1() {
+        pointcut.setExpression("execution(* com.ttasjwi.aop.member.MemberServiceImpl.hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("패키지 이름 정확히 + 클래스명, 메서드명 생략")
+    void packageExactMatch2() {
+        pointcut.setExpression("execution(* com.ttasjwi.aop.member.*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("패키지 명 뒤에 '.' -> 직계 하위 패키지가 아니므로 실패")
+    void packageExactFalse() {
+        pointcut.setExpression("execution(* com.ttasjwi.aop.*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    @DisplayName("com.ttasjwi.aop.member 하위 매핑")
+    void packageMatchSubPackage1() {
+        pointcut.setExpression("execution(* com.ttasjwi.aop.member..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("com.ttasjwi.aop 하위 매핑")
+    void packageMatchSubPackage2() {
+        pointcut.setExpression("execution(* com.ttasjwi.aop..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
 }
