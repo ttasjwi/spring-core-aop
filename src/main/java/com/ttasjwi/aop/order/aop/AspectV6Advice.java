f5596737535b6457af81874f79721f5233ad42b8
ttasjwi <ttasjwi920@gmail.com>
2022-09-28 19:04:36 +0900

    AOP 구현 6 - 어드바이스들 종류별 실행 순서

--- src/main/java/com/ttasjwi/aop/order/aop/AspectV6Advice.java
+++ src/main/java/com/ttasjwi/aop/order/aop/AspectV6Advice.java
@@ -9,36 +9,33 @@ import org.aspectj.lang.annotation.*;
 @Aspect
 public class AspectV6Advice {
 
-    // com.ttasjwi.aop.order 패키지와 하위 패키지 이면서 클래스 이름이 *Service
     @Around("com.ttasjwi.aop.order.aop.Pointcuts.orderAndService()")
     public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
+            log.info("[around] [트랜잭션 시작] {}", joinPoint.getSignature());
             // @Before
-            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
             Object result = joinPoint.proceed();
             // @AfterReturning
-            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            log.info("[around] [트랜잭션 커밋] {}", joinPoint.getSignature());
             return result;
         } catch (Exception e) {
-
             // @AfterThrowing
-            log.error("[트랜잭션 롤백] {}", joinPoint.getSignature());
+            log.error("[around] [트랜잭션 롤백] {}", joinPoint.getSignature());
             throw e;
         } finally {
-
             // @After
-            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+            log.info("[around] [리소스 릴리즈] {}", joinPoint.getSignature());
         }
     }
 
     @Before("com.ttasjwi.aop.order.aop.Pointcuts.orderAndService()")
     public void doBefore(JoinPoint joinPoint) {
-        log.info("[Before] {}", joinPoint.getSignature());
+        log.info("[before] {}", joinPoint.getSignature());
     }
 
     @AfterReturning(value = "com.ttasjwi.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
     public void doReturn(JoinPoint joinPoint, Object result) {
-        log.info("[Return] {} return = {}", joinPoint.getSignature(), result);
+        log.info("[return] {} return = {}", joinPoint.getSignature(), result);
     }
 
     @AfterThrowing(value = "com.ttasjwi.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
