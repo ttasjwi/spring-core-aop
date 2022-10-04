819bbcbb16bdb2b7e25e4a2b5376e0229de07c73
ttasjwi <ttasjwi920@gmail.com>
2022-10-04 17:34:49 +0900

    프록시와 내부호출 - 대안1 : 자기 자신을 setter로 주입

---
+++ src/main/java/com/ttasjwi/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,27 @@
+package com.ttasjwi.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("callServiceV1 setter={}", callServiceV1.getClass()); // 생성자를 통한 초기화 이후 setter로 프록시 주입
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external() {
+        log.info("call external");
+        callServiceV1.internal(); // 외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
